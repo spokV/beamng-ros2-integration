1abdd69e76400ffbbdc4726005f1c1cd686923a4
spokV <allacademic.grid358@passmail.net>
2025-08-29 16:38:19 +0300

    chore: remove deprecated LLM prompt client script

--- scripts/run_llm_prompt_client.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/jazzy/setup.bash
-source /home/spok/ros2_ws/install/setup.bash
-
-python3 -m beamng_ros2.examples.vision_llm_client \
-    --config /home/spok/ros2_ws/src/beamng-ros2-integration/configs/vision_prompt_config.json \
-    --interval 10.0
